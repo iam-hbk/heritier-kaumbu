6deceff27cf8a71ced7a6840ab124e351dd85e70
iam-hbk <rainheritier@gmail.com>
2024-08-07 12:54:31 +0200

    metadata

---
+++ src/app/_metadata.ts
@@ -0,0 +1,17 @@
+// app/_metadata.js or app/_metadata.ts
+
+export const metadata = {
+  openGraph: {
+    title: "Heritier Kaumbu",
+    description: "Hi there! I'm Heritier Kaumbu, a software engineer.",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Heritier Kaumbu",
+      },
+    ],
+    type: "website",
+  },
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
